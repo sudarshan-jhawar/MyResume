d08507f0169356b3843f0bcc0495537521b8d856
sudarshan-jhawar <sudarshan891@gmail.com>
2018-09-15 09:20:12 +0530

    Initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-resume-app-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent {
+  title = 'my-resume-app';
+}
